e3332d324391f52f7b3ad9fe6027eb1f3cd1bfae
Rajat Sharma <rajat.sharma@mv.uni-kl.de>
2022-04-05 11:11:57 +0200

    added unit tests

--- test.py
+++ test.py
@@ -16,11 +16,24 @@ class FlaskTest(unittest.TestCase):
         response = tester.get("/")
         self.assertEqual(response.status_code, 200)
 
-    # Check if response is JSON
+    # Check if the response of the job_status api is application/json
     def test_index_content_type(self):
         tester = app.test_client(self)
-        response = tester.get("/")
+        response = tester.get("/job_status")
         self.assertIn("application/json", response.content_type)
+
+     # Check if the response of the build_push_image api is HTML
+    def test_content_type(self):
+        tester = app.test_client(self)
+        response = tester.get("/build_push_image")
+        self.assertIn("text/html", response.content_type)
+
+
+    # Check for Data returned from the job_status api   
+    def test_index_data(self):
+        tester = app.test_client(self)
+        response = tester.get("/job_status")
+        self.assertTrue(b"message" in response.data)
 
 
 if __name__ == "__main__":
